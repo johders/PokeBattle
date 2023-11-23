3c01cb526361dd9d49abd688f268cbcd872db0ab
jd-HW <johannes.dereuddre@student.howest.be>
2023-11-23 09:06:31 +0100

    Add class library and folder structure

---
+++ Pra.Pe1.PokeBattle.Core/Services/BattleService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pra.Pe1.PokeBattle.Core.Services
+{
+    public class BattleService
+    {
+    }
+}
